6521684ac8021dbf1a630f8ada5d027a0d482be6
Leonid Stoyko <gribojor@gmail.com>
2026-02-27 12:45:41 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import datetime
 import pandas
 from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
-from pprint import pprint
 from collections import defaultdict
+
 
 def company_age():
     return datetime.date.today().year - 1920
@@ -22,12 +22,16 @@ def age_format(years):
 
 def main():
     age = company_age()
-    wine_catalog = pandas.read_excel(io='wine3.xlsx', na_values='nan', keep_default_na=False)
+    wine_catalog = pandas.read_excel(
+        io='wine3.xlsx',
+        na_values='nan',
+        keep_default_na=False
+    )
     wines = wine_catalog.to_dict('records')
-    
-    wine_cat = defaultdict(list)  
+
+    wine_cat = defaultdict(list)
     for wine in wines:
-        category = wine.get('Категория')  
+        category = wine.get('Категория')
         wine_cat[category].append(wine)
     for category in wine_cat:
         wine_cat[category].sort(key=lambda x: x.get('Цена', 0))
@@ -36,15 +40,16 @@ def main():
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
     )
-    
+
     template = env.get_template('template.html')
     rendered_page = template.render(
         wine_cat=wine_cat,
         years_old=age_format(age)
     )
-    
+
     with open('index.html', 'w', encoding="utf8") as file:
         file.write(rendered_page)
+
 
 if __name__ == "__main__":
     main()
